221ae0299a2638319ec521d1c1d993e6094718ed
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2024-08-18 13:57:50 +0200

    ♻️ Update : add function to render the text dynamically AND IMPORT image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+const reactDescription = ['Base', 'Main', 'Core'];
+function getRandomIndex(max){
+  return Math.floor(Math.random() * (max+1));
+}
 function App() {
+  const randomIndex = reactDescription[getRandomIndex(2)];
   return (
     <div>
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+         <b> {randomIndex} </b>React concepts you will need for almost any app you are
           going to build!
         </p>
       </header> 
